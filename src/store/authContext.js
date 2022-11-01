4e6c37b5360c9e0c30e04fdf11ebc7d24666f4d1
Danny Ramirez <dcr1999jr@gmail.com>
2022-10-31 21:26:22 -0700

    proj4-part1-assistance-needed

--- src/store/authContext.js
+++ src/store/authContext.js
@@ -19,12 +19,15 @@ const calculateRemainingTime = (exp) => {
 const getLocalData = () => {
   const storedToken = localStorage.getItem('token')
   const storedExp = localStorage.getItem('exp')
+  const storedId = localStorage.getItem('id')
+
 
   const remainingTime = calculateRemainingTime(storedExp)
 
   if (remainingTime <= 1000 * 60 * 30) {
     localStorage.removeItem('token')
     localStorage.removeItem('exp')
+    localStorage.removeItem('id')
     return null
   }
 
@@ -32,6 +35,7 @@ const getLocalData = () => {
   return {
     token: storedToken,
     duration: remainingTime,
+    userId:storedId
   }
 }
 
@@ -41,17 +45,45 @@ export const AuthContextProvider = (props) => {
   const localData = getLocalData()
   
   let initialToken
+  let initialId
   if (localData) {
     initialToken = localData.token
+    initialId = localData.userId
   }
 
   const [token, setToken] = useState(initialToken)
-  const [userId, setUserId] = useState(null)
+  const [userId, setUserId] = useState(initialId)
 
 
-  const logout = () => {}
+  const logout = useCallback(() => {
+    setToken(null)
+    setUserId(null)
+    localStorage.removeItem('token')
+    localStorage.removeItem('exp')
+    localStorage.removeItem('userId')
 
-  const login = () => {}
+    if (logoutTimer) {
+      clearTimeout(logoutTimer)
+    }
+  }, [])
+
+  const login = (token,exp,userId) => {
+    setToken(token)
+    setUserId(userId)
+    localStorage.setItem('token', token)
+    localStorage.setItem('userID', userId)
+    localStorage.setItem('exp', exp)
+
+    const remainingTime = calculateRemainingTime(exp)
+
+    logoutTimer = setTimeout(logout,remainingTime)
+  }
+
+  useEffect(() => {
+    if (localData) {
+      logoutTimer = setTimeout(logout, localData.duration)
+    }
+  }, [localData, logout])
 
   const contextValue = {
     token,
